0bf8b0a2b9b558ed5a2a1bba6d6eeafbc0a53992
Nitisha <nitishagupta.1999@gmail.com>
2021-12-02 18:06:39 +0530

    feat: functionality done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import react, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <div className="container">
+      <h1>Questions and answers about login</h1>
+      <section className="info">
+        {questions.map((question) => {
+          return <SingleQuestion key={question.id} {...question} />;
+        })}
+      </section>
+    </div>
+  );
+}
+
+export default App;
--- src/Question.jsx
+++ src/Question.jsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-export default function Question() {
-  return <div></div>;
+export default function Question({ title, info }) {
+  const [showInfo, setshowInfo] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setshowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
+  );
 }
