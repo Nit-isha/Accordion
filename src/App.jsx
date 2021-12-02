50f8e46753f2e6a286ba7ab33603dcda1deeb83c
Nitisha <nitishagupta.1999@gmail.com>
2021-12-02 18:11:19 +0530

    feat: css done

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [questions, setQuestions] = useState(data);
   return (
     <div className="container">
-      <h1>Questions and answers about login</h1>
+      <h3>Questions and answers about login</h3>
       <section className="info">
         {questions.map((question) => {
           return <SingleQuestion key={question.id} {...question} />;
